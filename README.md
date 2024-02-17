4c7ee03e513869a679a2696afe3a513741830c97
monaewhite <monaemwhite@gmail.com>
2024-02-17 12:49:20 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # cunyhack
+
+**Authors: Monae White and Tamanna Chowdhury**
+
+This project aims to simplify the process of creating a class schedule for computer science college students. It provides an intuitive interface for users to input their specific requirements and preferences regarding computer science classes, math classes, and common core requirements. The generator then produces a personalized class schedule, incorporating both user-provided choices and randomly selected classes that fulfill common core requirements.
+
+This project is based on the course selections at CUNY Hunter College for a student majoring in computer science.
+
+##Features
+**User Input:** Easily input computer science classes, math classes, and common core requirements.
+**Customization:** Tailor the generated schedule to match individual preferences and academic needs.
+**Random Selection:** Automatically selects one or two classes from a predefined list based on common core requirements.
+**Calendar Display:** Presents the generated class schedule in a visually appealing calendar format on a webpage.
--- index.py
+++ index.py
@@ -3,79 +3,65 @@ import socketserver
 from http import HTTPStatus
 import json
 
-
 class Handler(http.server.SimpleHTTPRequestHandler):
 
-    preferences = {}
+    user_data = []
 
-    def do_POST(self): 
+    def do_GET(self):
+        # Set the required headers for the GET request.
+        self.send_response(HTTPStatus.OK)
+
+        if self.path == '/':
+            # Serve the HTML file
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+            with open("index.html", "rb") as file:
+                self.wfile.write(file.read())
+        else:
+            # Serve other files (CSS, JS, etc.) using the default handler
+            super().do_GET()
+
+    def do_POST(self):
         # Set the required headers for the POST request.
-        self.send_response(200)
+        self.send_response(HTTPStatus.OK)
         self.send_header("Content-type", "application/json")
         self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
 
-        # Read the body of the request. This is the "body" we set in the Javascript code.
+        # Read the body of the request. This is the "body" we set in the JavaScript code.
         content_length = int(self.headers["Content-Length"])
         body = self.rfile.read(content_length)
         body_message = json.loads(body)
 
         # Do some processing logic. This is the "meat and potatoes" of your app.
-        print("Recieved Request: ")
+        print("Received Request:")
         print(body_message)
 
-        
+        # Extract user information from the request and update preferences
+        user_info = {
+            "cs_course": body_message.get("cs_course"),
+            "math_course": body_message.get("math_course"),
+            "hunter_courses": body_message.get("hunter_courses")
+        }
 
-        if (self.path == '/songs'):
-            print('/songs')
-            song = body_message['song']
+        # Store the user information in the list
+        self.user_data.append(user_info)
 
-            if song in self.preferences:
-                self.preferences[song]['upvote'] += 1
-            else:
-                self.preferences[song] = {"upvote": 1, "downvote": 0} #struct for upvote in a map form and can store many things 
-        # Write a response
-            self.wfile.write(json.dumps({"preferences": self.getPreference()}).encode())
+        # Send a response indicating successful storage
+        response_data = {"status": "success", "message": "User information stored successfully"}
+        self.wfile.write(json.dumps(response_data).encode("utf-8"))
 
-        elif (self.path == '/votes'):
-            song = body_message['song']
-            isUpvote = body_message['isUpvote']
-
-            if (isUpvote):
-                self.preferences[song]['upvote'] += 1 #getting the feild and adding 1
-            else:
-                self.preferences[song]['downvote'] += 1
-                
-            print(song)   
-            self.wfile.write(json.dumps({"preferences": self.getPreference()}).encode())
-
-            
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
+    def do_OPTIONS(self):
+        # Set the required headers for the preflight request.
+        self.send_response(HTTPStatus.OK)
         self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.send_header("Access-Control-Max-Age", "86400")
         self.end_headers()
-        
 
-        if(self.path == '/votes'):
-            print('hi')
-            self.wfile.write(json.dumps({"preferences": self.getPreference()}).encode())
-            return 
-        self.wfile.write(json.dumps({}).encode())
-        print('hi2')
-
-
-    def getPreference(self):
-        # return list of song names in order 
-        # return an array of objects
-        # [{song: '', vote: int}, {song: '', vote: int}, {song: '', vote: int}, {song: '', vote: int}]
-        songList = [{'song': song, 'vote': self.preferences[song]} for song in self.preferences]
-        sorted_songList = sorted(songList, key = lambda songStruct: songStruct['vote']['upvote'] - songStruct['vote']['downvote'])
-        sorted_songList.reverse()
-        return sorted_songList
-
-
-
+# Run the server
 port_num = 8000
 httpd = socketserver.TCPServer(("", port_num), Handler)
 print("Starting Up on Port " + str(port_num))
